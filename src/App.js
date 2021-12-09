200d9024eb8eb92a62aa62d9c0d62e7202c5751c
Mindaugas Jačionis <m.jacionis@gmail.com>
2021-12-09 18:06:31 +0200

    Add few components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Separator from "./components/separator";
 import Header from "./components/header";
+import Pill from "./components/pill";
+import ContentBox from "./components/content-box";
 
 import linkedin from "./images/linkedin-logo.png";
 import twitter from "./images/twitter-logo.png";
@@ -13,93 +15,73 @@ function App() {
       <Header />
       <main className="main">
         <section className="section">
-          <div className="content-box flex-1">
-            <h3 className="content-box__title">Links</h3>
-            <Separator isFancy />
-            <div className="content-box__content">
-              <ul className="social-list">
-                <li className="social-list__item">
-                  <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
-                    <img src={linkedin} alt="linkeding logo" />
-                    <p>Linkedin/UserName</p>
-                  </a>
-                </li>
-                <li className="social-list__item">
-                  <a href="https://twitter.com/mjacionis" target="_blank" rel="noreferrer">
-                    <img src={twitter} alt="twitter logo" />
-                    <p>@MJacionis</p>
-                  </a>
-                </li>
-                <li className="social-list__item">
-                  <a href="https://github.com/mindaugas-jacionis" target="_blank" rel="noreferrer">
-                    <img src={github} alt="github logo" />
-                    <p>mindaugas-jacionis</p>
-                  </a>
-                </li>
-                <li className="social-list__item">
-                  <a href="https://dev.to/" target="_blank" rel="noreferrer">
-                    <img src={blog} alt="dev.to logo" />
-                    <p>dev.to blog</p>
-                  </a>
-                </li>
-              </ul>
+          <ContentBox className="flex-1" title="Links">
+            <ul className="social-list">
+              <li className="social-list__item">
+                <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
+                  <img src={linkedin} alt="linkeding logo" />
+                  <p>Linkedin/UserName</p>
+                </a>
+              </li>
+              <li className="social-list__item">
+                <a href="https://twitter.com/mjacionis" target="_blank" rel="noreferrer">
+                  <img src={twitter} alt="twitter logo" />
+                  <p>@MJacionis</p>
+                </a>
+              </li>
+              <li className="social-list__item">
+                <a href="https://github.com/mindaugas-jacionis" target="_blank" rel="noreferrer">
+                  <img src={github} alt="github logo" />
+                  <p>mindaugas-jacionis</p>
+                </a>
+              </li>
+              <li className="social-list__item">
+                <a href="https://dev.to/" target="_blank" rel="noreferrer">
+                  <img src={blog} alt="dev.to logo" />
+                  <p>dev.to blog</p>
+                </a>
+              </li>
+            </ul>
+          </ContentBox>
+          <ContentBox className="flex-2" title="About Me">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
+              Vestibulum vitae mattis diam. Vivamus eleifend diam vel tempor lacinia. Suspendisse
+              non augue egestas, dapibus justo et, lobortis ex. Vestibulum vitae mattis diam.
+              Vivamus eleifend diam vel tempor lacinia. Suspendisse non augue egestas, dapibus justo
+              et, lobortis ex.
+            </p>
+          </ContentBox>
+        </section>
+        <section className="section">
+          <ContentBox className="flex-1" title="Education">
+            <div className="school-info">
+              <p>School Name</p>
+              <p>2003 - 2015</p>
+              <p>Some Diploma</p>
             </div>
-          </div>
-          <div className="content-box flex-2">
-            <h3 className="content-box__title">About Me</h3>
-            <Separator isFancy />
-            <div className="content-box__content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut justo libero.
-                Vestibulum vitae mattis diam. Vivamus eleifend diam vel tempor lacinia. Suspendisse
-                non augue egestas, dapibus justo et, lobortis ex. Vestibulum vitae mattis diam.
-                Vivamus eleifend diam vel tempor lacinia. Suspendisse non augue egestas, dapibus
-                justo et, lobortis ex.
-              </p>
+            <Separator isShort />
+            <div className="school-info">
+              <p>School Name</p>
+              <p>2003 - 2015</p>
+              <p>Some Diploma</p>
             </div>
+          </ContentBox>
+          <div className="d-flex flex-2">
+            <ContentBox title="Personal Skills" className="flex-1 pills-box">
+              <Pill color="green">Teamwork</Pill>
+              <Pill color="yellow">Communication</Pill>
+              <Pill color="red">Organization</Pill>
+            </ContentBox>
+            <ContentBox className="flex-1 pills-box" title="Technical Skills">
+              <Pill color="green">HTML</Pill>
+              <Pill color="yellow">CSS</Pill>
+              <Pill color="green">JavaScript</Pill>
+            </ContentBox>
           </div>
         </section>
         <section className="section">
-          <div className="content-box flex-1">
-            <h3 className="content-box__title">Links</h3>
-            <Separator isFancy />
-            <div className="content-box__content">
-              <div className="school-info">
-                <p>School Name</p>
-                <p>2003 - 2015</p>
-                <p>Some Diploma</p>
-              </div>
-              <Separator isShort />
-              <div className="school-info">
-                <p>School Name</p>
-                <p>2003 - 2015</p>
-                <p>Some Diploma</p>
-              </div>
-            </div>
-          </div>
-          <div className="content-box flex-1 pills-box">
-            <h3 className="content-box__title">Personal Skills</h3>
-            <Separator isFancy />
-            <div className="content-box__content d-inline-flex flex-column">
-              <span className="pill pill--green">Teamwork</span>
-              <span className="pill pill--yellow">Communication</span>
-              <span className="pill pill--red">Organization</span>
-            </div>
-          </div>
-          <div className="content-box flex-1 pills-box">
-            <h3 className="content-box__title">Technical Skills</h3>
-            <Separator isFancy />
-            <div className="content-box__content d-inline-flex flex-column">
-              <span className="pill pill--green">HTML</span>
-              <span className="pill pill--yellow">CSS</span>
-              <span className="pill pill--green">JavaScript</span>
-            </div>
-          </div>
-        </section>
-        <section className="section">
-          <div className="content-box flex-1">
-            <h3 className="content-box__title">Links</h3>
-            <Separator isFancy />
+          <ContentBox className="flex-1" title="Experience">
             <div className="content-box__content job-experience">
               <div className="experience">
                 <div className="experience__meta">
@@ -153,7 +135,7 @@ function App() {
                 </ul>
               </div>
             </div>
-          </div>
+          </ContentBox>
         </section>
       </main>
       <footer className="footer">
